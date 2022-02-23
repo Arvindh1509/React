d2555c1b660be856d1d92fdf7084bcc25dc9e18e
Arvindh1509 <rockstararvindh@gmail.com>
2022-02-23 19:46:48 +0530

    further added

--- 3.Whatsapp/whatsapp/src/Chat.js
+++ 3.Whatsapp/whatsapp/src/Chat.js
@@ -1,9 +1,63 @@
+import SearchOutlined from '@mui/icons-material/SearchOutlined'
+import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import "./Chat.css"
+import AttachFile from '@mui/icons-material/AttachFile'
+import MoreVert from '@mui/icons-material/MoreVert'
+import MicIcon from '@mui/icons-material/Mic'
+import InsertEmoticon from "@mui/icons-material/InsertEmoticon";
+
 
 function Chat() {
   return (
-    <div className='chat'> Chat</div>
+    <div className='chat'> 
+      <div className='chat_header'>
+        <Avatar/>
+        <div className='chatheader_info'>
+          <h2>room name</h2>
+          <p>last seen</p>
+        </div>
+        <div className='chatheader_right'>
+          <IconButton>
+            <SearchOutlined/>
+          </IconButton>
+          <IconButton>
+            <AttachFile/>
+          </IconButton>
+          <IconButton>
+            <MoreVert/>
+          </IconButton>
+        </div>
+      </div>
+      <div className='chat_body'>
+        <p className='chat_message'>
+        <span className='chat_name'>Arvindh</span>
+          
+          This is a message
+          <span className='chat_time'>
+            {new Date().toUTCString()}
+          </span>          
+          </p>
+
+          <p className='chat_message chat_receiver'>
+        <span className='chat_name'>Arvindh</span>
+          
+          This is a message
+          <span className='chat_time'>
+            {new Date().toUTCString()}
+          </span>                
+          </p>
+      </div>
+      <div className='chat_footer'>
+        <InsertEmoticon/>
+        <form >
+          <input placeholder='type a message'
+          type="text"/>
+          <button type='submit'>Send a message</button>
+        </form>
+        <MicIcon/>
+      </div>
+    </div>
   )
 }
 
--- 3.Whatsapp/whatsapp/src/Sidebar.js
+++ 3.Whatsapp/whatsapp/src/Sidebar.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import "./Sidebar.css";
-
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import { Avatar, IconButton } from '@mui/material';
+import  ChatIcon from "@mui/icons-material/Chat";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SidebarChat from './SidebarChat';
 function sidebar() {
   return (
     <div className='sidebar'>
-        <h1>im a sidebar</h1>
+        
+        <div className='sidebar_header'>
+          <Avatar src="https://i.guim.co.uk/img/media/9bbaa85c91fe55de14f83e11ee33e7352388bfdd/430_195_2987_1793/master/2987.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=f9fc787e98bae78e398c432ffb789394"
+          />
+          <div className='sidebar_headright'>
+            <IconButton>
+               <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+               <ChatIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+           </div>
+        </div>
+        <div className='sidebar_search'>
+          <div className='sidebar_searchcontainer'>
+            <SearchOutlined />
+            <input placeholder='=search or start a new chat' type="text"/   >
+          </div>
+
+        </div>
+        <div className='sidebar_chats'>
+          <SidebarChat/>
+          <SidebarChat/>
+          <SidebarChat/>
+        </div>
     </div>
   )
 }
